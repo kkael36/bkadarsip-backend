9354c7817b14b0cdc38f3720eb67f1b18e412f11
Dika H.P <dikahadip4@gmail.com>
2026-04-21 11:30:56 +0700

    Fix: Explicit cloudinary config and variables

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,20 +1,13 @@
 <?php
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Cloudinary Configuration
-    |--------------------------------------------------------------------------
-    */
     'cloud_url' => env('CLOUDINARY_URL'),
-
-    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
-
-    'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
 
     'cloud' => [
         'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
         'api_key'    => env('CLOUDINARY_API_KEY'),
         'api_secret' => env('CLOUDINARY_API_SECRET'),
     ],
+
+    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
 ];
