7daa10583188dcfa5e150a2f8dd562736d048b5a
Dika H.P <dikahadip4@gmail.com>
2026-04-21 11:38:56 +0700

    Fix: cloudinary dan app service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,12 @@ class AppServiceProvider extends ServiceProvider
     if (config('app.env') !== 'local') {
         URL::forceScheme('https');
     }
+
+    // PAKSA CONFIG CLOUDINARY
+    config(['cloudinary.cloud' => [
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key'    => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET'),
+    ]]);
 }
 }
--- config/cloudinary.php
+++ config/cloudinary.php
@@ -4,9 +4,9 @@ return [
     'cloud_url' => env('CLOUDINARY_URL'),
 
     'cloud' => [
-        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-        'api_key'    => env('CLOUDINARY_API_KEY'),
-        'api_secret' => env('CLOUDINARY_API_SECRET'),
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME', 'dswy4tagj'),
+        'api_key'    => env('CLOUDINARY_API_KEY', '877393947668591'),
+        'api_secret' => env('CLOUDINARY_API_SECRET', 'h-EXj0-IhNHx2zKBuNXVwNbPeWI'),
     ],
 
     'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
